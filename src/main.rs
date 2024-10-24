a899c538ec283a68370601ac06afdd0906a351d5
Avaneesh3 <avanya.3@gmail.com>
2024-10-24 16:50:50 +0530

    Exercise 01 Part 01 for Scientific Computing

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // This is exercise 01 Part 1 of the Scientific Computing Course
+    // The function prints "Your Name: Hello Scientific Computing"
+    // Here my name (Avaneesh) is given in my native script Devanagari
+
+    let name_in_native_script = "अवनीश";
+    println!("{name_in_native_script}: Hello Scientific Computing");
+
+}
